c30c80d0b63fec80af7120087dfb473603ad5a0a
Daniel Yde Therkildsen <36370310+StewyEarth@users.noreply.github.com>
2019-02-07 08:55:16 +0100

    Opgave Done

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Tilføj et DOMContentLoaded event
+document.addEventListener("DOMContentLoaded",()=>{
+
+    // Skift baggrundsfarven på "#target" elementet, når siden er loadet.
+        let targetElem = document.querySelector("#target");
+        targetElem.style.backgroundColor = "blue";
+    
+    }); // DOMContentLoaded End
+    
+    // Aflevér på GitHub ved at Committe og Pushe.

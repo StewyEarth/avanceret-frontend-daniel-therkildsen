206cce1e59fabc4438fa51ced48d2ffda6db4f38
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-03-21 13:54:12 +0100

    Changed some clone node things to make it more correct

--- del2/03_clonenode/js/script.js
+++ del2/03_clonenode/js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let itemElem = document.querySelector(".item");
+    let itemTemplateElem = document.querySelector(".html-templates .item");
     let bodyElem = document.querySelector("body");
 
     let titleArray = ["Taxi trip","Beach trip","Airplane ride"]
 
     titleArray.forEach(title => {
-        clone = itemElem.cloneNode(true);
-        clone.querySelector(".item-title").innerHTML = title;
-        bodyElem.appendChild(clone);
+        let itemClone = itemTemplateElem.cloneNode(true); //When true it's allowed child notes.
+        itemClone.querySelector(".item-title").innerHTML = title;
+        bodyElem.appendChild(itemClone);
     });
 
 });

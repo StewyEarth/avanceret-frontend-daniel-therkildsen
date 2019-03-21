8054641e883ff2bf44721d82476c1c5919d9eb6e
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-03-21 10:26:00 +0100

    Bedre navngivning

--- del2/01_html-partials/js/navload.js
+++ del2/01_html-partials/js/navload.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let headerElem = document.querySelector("header")
     fetch('partials/nav-partial.html')
         .then((response) => { return response.text() })
-        .then((data) => {
-            headerElem.innerHTML = data;
+        .then((navHTML) => {
+            headerElem.innerHTML = navHTML;
         });
 });
